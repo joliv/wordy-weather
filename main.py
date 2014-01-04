0abd2ca639207d94d74233bdf95160f48488c967
joliv <jward97@gmail.com>
2014-01-04 18:54:22 -0500

    Added zip verification against zip list

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import render_template
+import zip_codes
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/<zip_code>")
+def forecast(zip_code):
+	if not zip_codes.is_listed(zip_code):
+		return render_template("wrong_zip.html", zip_code=zip_code)
+	pass
 
 if __name__ == "__main__":
     app.run(debug=True)
