29f2fe87622e1a43c81507b86706e20ad86a5b6e
joliv <jward97@gmail.com>
2014-01-11 17:18:21 -0500

    Added station and zone lookup

--- zip_codes.py
+++ zip_codes.py
@@ -8,3 +8,10 @@ def is_listed(zip_code):
 	if c.fetchone() is None:
 		return False
 	return True
+
+def station_zone(zip_code):
+	query = (zip_code,)
+	conn = sqlite3.connect('zip_codes.db')
+	c = conn.cursor()
+	c.execute('SELECT station,zone FROM lookup WHERE zipcode=?',query)
+	return c.fetchone()
