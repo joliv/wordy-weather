50c5e6c9b6b01a53c756dad3265a0e637dccff58
joliv <jward97@gmail.com>
2014-01-19 19:15:24 -0500

    Added NWS forecast parsing

--- weather.py
+++ weather.py
@@ -8,3 +8,39 @@ def forecast_txt(zip_code):
 	page = request.urlopen(url)
 	txt = [line.decode() for line in page]
 	return txt
+
+def day_range(txt):
+	for index,line in enumerate(txt):
+		if line!='' and line.startswith('.'):
+			begin = index
+			end = txt.index('$$')-1
+			return begin,end
+
+def parse(txt):
+	txt = [line.replace('\n','').strip(' ')
+		for line in txt]
+	start,end = day_range(txt)
+	forecast = txt[start:end]
+	day_indices = [(i) for i,line in enumerate(forecast)
+		if line.startswith('.')]
+	grouped = [
+		' '.join(forecast[day_index:day_indices[index+1]])
+		for index,day_index in enumerate(day_indices)
+		if index != len(day_indices)-1
+	]
+	grouped.append(
+		' '.join(forecast[day_indices[-1]:])
+	)
+	cleaned = [
+		line.lstrip('.')
+		for line in grouped
+	]
+	split = [
+		line.split('...')
+		for line in cleaned
+	]
+	regrouped = [
+		(line[0],"—".join(line[1:]))
+		for line in split
+	]
+	return regrouped
