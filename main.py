5c613ae0a6b55d8417412a03ca00fd52eb53130e
joliv <jward97@gmail.com>
2014-01-19 20:24:04 -0500

    Added forecast page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import zip_codes
+import weather
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +12,11 @@ def index():
 def forecast(zip_code):
 	if not zip_codes.is_listed(zip_code):
 		return render_template("wrong_zip.html", zip_code=zip_code)
-	pass
+	city = "/".join(zip_codes.city(zip_code))
+	txt = weather.forecast_txt(zip_code)
+	forecast = weather.parse(txt)
+	print(forecast)
+	return render_template("forecast.html",city=city,forecast=forecast)
 
 if __name__ == "__main__":
     app.run(debug=True)
